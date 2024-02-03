44d64097b9244bda0135d5e845eb770690ecc32a
Arpit Goyal <arpit.go4@gmail.com>
2024-02-03 21:13:11 +0530

    Adds solutions for,
    * Flight Discount & Flight Routes (SSSP, Graph)

--- Graph Algorithms/Flight Discount (CSES).cpp
+++ Graph Algorithms/Flight Discount (CSES).cpp
@@ -3,39 +3,89 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include <tuple>
 #include <queue>
 
 using namespace std;
 
-vector<vector<pair<int,int>>> G;
-vector<int> VIS;
-vector<int> DIST;
+typedef long long ll;
+typedef tuple<ll,int,bool> state;
+typedef pair<int,ll> edge;
 
-void dijkstra(vector<vector<int>>& G, int source) {
-    auto comp = [&](const pair<int,int>& p1, const pair<int,int>& p2) {
-        return p1.first > p2.first;
+// Time: O(V + 2ElogV)
+// Space: O(V * 2) ~ O(V)
+
+/**
+ * We can solve this problem with simple recursion and speed it up with DP.
+ * As problem, has an optimal substructure to it.
+ * DP state: { vertex, coup_applied }
+ * 
+ * Dijkstra's algorithm is greedy algorithm. Greedy algorithm are also based on
+ * problems having optimal substructure. We can extend Dijkstra's algorithm to solve
+ * this problem by keeping the, 
+ * state: { vertex_dist_from_source, vertex, coup_applied }
+ * 
+ * DP based approach is based on DFS traversal. And Dijkstra's is can be loosely said to 
+ * be based on BFS traversal, only difference rather than having a vanilla queue, we 
+ * are using a priority queue.
+*/
+
+vector<vector<pair<int,ll>>> G;
+vector<vector<int>> dp;
+
+void dijkstra(vector<vector<edge>>& G, int src, int dest) {
+    auto comp = [&](state& s1, state& s2) {
+        return get<0>(s1) > get<0>(s2);
     };
-    priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(comp)> pq(comp);
-    DIST[source] = 0;
-    pq.push({ DIST[source], source });
+    priority_queue<state, vector<state>, decltype(comp)> pq(comp);        // min-heap
+
+    ll min_price = LLONG_MAX;
+    pq.push({ 0, src, false });
 
     while (!pq.empty()) {
+        state p = pq.top();
+        pq.pop();
 
+        ll d; int u; bool coup_used;
+        tie(d,u,coup_used) = p;
+
+        // condition to avoid adding vertices again and again!
+        // or, to avoid repeating same states over again!
+        // problem state: (vertex, coup_used)
+        if (dp[u][coup_used] == 1)
+            continue;
+
+        dp[u][coup_used] = 1;
+
+        if (u == dest)
+            min_price = min(min_price, d);
+        
+        // relax neighbouring vertices
+        for (edge& e : G[u]) {
+            int v = e.first; ll w = e.second;
+            if (coup_used)
+                pq.push({ d + w, v, true });
+            else {
+                pq.push({ d + (w/2), v, true });
+                pq.push({ d + w, v, false });
+            }
+        }
     }
+
+    cout << min_price;
 }
 
-void solve(int V, int E, vector<tuple<int,int,int>>& edges) {
-    G.assign(V+1, vector<pair<int,int>>());
-    VIS.assign(V+1, 0);
-    DIST.assign(V+1, INT_MAX);
+void solve(int V, int E, vector<tuple<int,int,ll>>& edges) {
+    G.assign(V+1, vector<pair<int,ll>>());
+    dp.assign(V+1, vector<int>(2, -1));
 
-    for (tuple<int,int,int>& t : edges) {
-        int u, v, w;
+    for (tuple<int,int,ll>& t : edges) {
+        int u, v; ll w;
         tie(u,v,w) = t;
         G[u].push_back({ v, w });
     }
 
-    dijkstra(G, 1);
+    dijkstra(G, 1, V);
 }
 
 int main() {
@@ -45,7 +95,7 @@ int main() {
     int V, E;
     cin >> V >> E;
 
-    vector<tuple<int,int,int>> edges(E);
+    vector<tuple<int,int,ll>> edges(E);
     int u, v, w;
     for (int i = 0; i < E; i++) {
         cin >> u >> v >> w;
---
+++ Graph Algorithms/Flight Routes (CSES).cpp
@@ -0,0 +1,76 @@
+// Flight Routes (CSES)
+ 
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <queue>
+#include <tuple>
+ 
+using namespace std;
+using ll = long long;
+
+// Time: O(V + KElogV) ~ O(KElogV)
+// Space: O(V)
+
+vector<vector<pair<int,ll>>> G;
+vector<int> counter;
+
+void dijkstra(int src, int K, int V) {
+    auto comp = [&](auto& p1, auto& p2) {
+        return p1.first > p2.first;
+    };
+    priority_queue<pair<ll,int>, vector<pair<ll,int>>, decltype(comp)> pq(comp);  // min-heap
+
+    pq.push({ 0, src });
+
+    while (!pq.empty() && counter[V] < K) {
+        auto p = pq.top();
+        pq.pop();
+
+        int u = p.second;
+        ll d = p.first;
+
+        counter[u]++;
+        if (u == V)
+            cout << d << " ";
+
+        if (counter[u] <= K) {
+            for (auto& e : G[u]) {
+                int v = e.first; ll w = e.second;
+                pq.push({ d+w, v });
+            }
+        }
+    }
+}
+
+void solve(int V, int E, int K, vector<tuple<int,int,int>>& edges) {
+    G.assign(V+1, vector<pair<int,ll>>());
+    counter.assign(V+1, 0);
+
+    for (tuple<int,int,int>& t : edges) {
+        int u, v; ll w;
+        tie(u,v,w) = t;
+        G[u].push_back({ v, w });
+    }
+
+    dijkstra(1, K, V);
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int V, E, K;
+    cin >> V >> E >> K;
+
+    vector<tuple<int,int,int>> edges(E);
+    int u, v, w;
+    for (int i = 0; i < E; i++) {
+        cin >> u >> v >> w;
+        edges[i] = { u, v, w };
+    }
+
+    solve(V, E, K, edges);   
+    
+    return 0;
+}
