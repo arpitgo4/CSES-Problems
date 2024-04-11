54ca7164105d389ba4e909bf980299367f903c43
AG1911765 <arpit.goyal@cerner.com>
2024-04-11 12:44:00 +0530

    Adds solution to problem,
    * Finding a Centroid (Tree, DFS, Centroid)

--- Tree Algorithms/Finding a Centroid (CSES).cpp
+++ Tree Algorithms/Finding a Centroid (CSES).cpp
@@ -1,77 +1,67 @@
 // Finding a Centroid (CSES)
-
+ 
 #include <iostream>
 #include <vector>
-
+ 
 using namespace std;
-
-// Time: O(N + (N-1)) ~ O(N)
-// Space: O(N + (N-1)) ~ O(N)
+ 
+// Time: O(N)
+// Space: O(N)
+ 
+typedef pair<int,int> edge;
 
 vector<vector<int>> T;
-vector<int> degree;
+vector<int> sz;
 
-int centroid_decomposition(vector<vector<int>>& T, int N) {
-    if (N < 3)
-        return 1;
-
-    vector<int> layer;
-    for (int u = 1; u <= N; u++)
-        if (degree[u] == 1)
-            layer.push_back(u);
-
-    int total_nodes = N;
-    while (total_nodes > 2) {
-
-        vector<int> next_layer;
-        for (int u : layer) {
-            for (int v : T[u]) {
-                degree[v]--;
-                if (degree[v] == 1)
-                    next_layer.push_back(v);
-            }
-
-            total_nodes--;
-        }
-
-        layer = next_layer;
+int find_centroid(int u, int p, int N) {
+    for (int v : T[u]) {
+        if (v != p && sz[v] > N/2)
+            return find_centroid(v, u, N);
     }
 
-    return layer[0];
+    return u;
 }
 
-void solve(int N, vector<pair<int,int>>& edges) {
-    T.assign(N+1, vector<int>());
-    degree.assign(N+1, 0);
+void dfs(int u, int p) {
+    sz[u] = 1;
+    for (int v : T[u]) {
+        if (v != p) {
+            dfs(v, u);
+            sz[u] += sz[v];
+        }
+    }
+}
 
-    for (pair<int,int>& e : edges) {
+void solve(vector<edge>& edges, int N) {
+    T.assign(N+1, vector<int>());
+    sz.assign(N+1, 0);
+    for (edge& e : edges) {
         int u = e.first, v = e.second;
         T[u].push_back(v);
         T[v].push_back(u);
-        degree[u] = T[u].size();
-        degree[v] = T[v].size();
     }
 
-    int centroid = centroid_decomposition(T, N);
+    dfs(1, -1);
 
-    cout << centroid;
+    int centroid = find_centroid(1, -1, N);
+    cout << centroid << endl;
 }
-
+ 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     int N;
     cin >> N;
 
-    vector<pair<int,int>> edges(N-1);
     int u, v;
+    vector<edge> edges(N-1);
     for (int i = 0; i < N-1; i++) {
         cin >> u >> v;
         edges[i] = { u, v };
     }
 
-    solve(N, edges);
-
+    solve(edges, N);
+    
     return 0;
 }
