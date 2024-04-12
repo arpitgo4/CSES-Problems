68553d2c2a860aeca0b57b027fb30231f199332e
AG1911765 <arpit.goyal@cerner.com>
2024-04-12 14:50:05 +0530

    Resolved problem,
    * Tree Diameter (Tree, DFS)

--- Tree Algorithms/Tree Diameter (CSES).cpp
+++ Tree Algorithms/Tree Diameter (CSES).cpp
@@ -2,54 +2,47 @@
 
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
 // Time: O(N)
 // Space: O(N)
 
+typedef pair<int,int> edge;
+
 vector<vector<int>> T;
-vector<int> VIS;
+
 int diameter = 0;
 
-int dfs(int u) {
-    VIS[u] = 1;
+int dfs(int u, int p) {
+    int max_1 = 0, max_2 = 0;
+    for (int v : T[u]) {
+        if (v != p) {
+            int dist = 1 + dfs(v, u);
+            if (dist > max_1) {
+                max_2 = max_1;
+                max_1 = dist;
+            } else if (dist > max_2)
+                max_2 = dist;
+        }
+    }
 
-    vector<int> dists;
-    for (int v : T[u])
-        if (VIS[v] == 0)
-            dists.push_back(1 + dfs(v));
+    diameter = max({ diameter, max_1, max_1 + max_2 });
 
-    priority_queue<int> pq(dists.begin(), dists.end());
-    
-    if (pq.empty())
-        return 0;
-
-    int a = pq.top();
-    pq.pop();
-
-    int b = 0;
-    if (!pq.empty())
-        b = pq.top();
-
-    diameter = max({ diameter, a + b });
-    return a;
+    return max_1;
 }
 
-void solve(vector<pair<int,int>>& E, int V) {
+void solve(vector<edge>& E, int V) {
     T.assign(V+1, vector<int>());
-    VIS.assign(V+1, 0);
-
-    for (pair<int,int>& p : E) {
+    for (edge& p : E) {
         int u = p.first, v = p.second;
         T[u].push_back(v);
         T[v].push_back(u);
     }
 
-    int max_dist = dfs(1);
+    dfs(1, -1);
 
-    cout << max(max_dist, diameter);
+    cout << diameter << endl;
 }
 
 int main() {
@@ -59,14 +52,14 @@ int main() {
     int N;
     cin >> N;
 
-    vector<pair<int,int>> E(N-1);
-    int a, b;
+    vector<edge> edges(N-1);
+    int u, v;
     for (int i = 0; i < N-1; i++) {
-        cin >> a >> b;
-        E[i] = { a, b };
+        cin >> u >> v;
+        edges[i] = { u, v };
     }
 
-    solve(E, N);
+    solve(edges, N);
 
     return 0;
 }
