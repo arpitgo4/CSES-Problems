ef344279fbf0f0b9023a189ac6dcfd6f83a2a025
Arpit Goyal <arpit.go4@gmail.com>
2024-08-24 03:39:58 +0530

    Adds solution to problem,
    * Range Update Queries (Segment Tree, Lazy Propagation)

--- Range Queries/Range Update Queries (CSES).cpp
+++ Range Queries/Range Update Queries (CSES).cpp
@@ -11,7 +11,6 @@ using namespace std;
 typedef long long ll;
 
 vector<ll> ST, A, lazy;
-vector<bool> marked;
 
 void build_seg_tree(int l, int h, int i) {
     if (l == h) {
@@ -27,25 +26,23 @@ void build_seg_tree(int l, int h, int i) {
 }
 
 void push(int i) {
-    if (marked[i]) {
+    if (lazy[i] != 0) {
         lazy[2*i+1] += lazy[i];
         lazy[2*i+2] += lazy[i];
         lazy[i] = 0;
-        marked[2*i+1] = marked[2*i+2] = true;
-        marked[i] = false;
     }
 }
 
-void update(int l, int h, int i, int p, int q, int x) {
+void update(int l, int h, int i, int p, int q, ll x) {
+    if (l > h || p > h || l > q)
+        return;
     if (l == p && h == q) {
-        marked[i] = true;
         lazy[i] += x;
         return;
     }
-    if (l == h)
-        return;
 
-    push(i);
+    if (l != h)                                                 // internal node
+        push(i);
 
     int m = (h-l)/2 + l;
     update(l, m, 2*i+1, p, min(m, q), x);
@@ -55,10 +52,13 @@ void update(int l, int h, int i, int p, int q, int x) {
 }
 
 ll query(int l, int h, int i, int k) {
+    if (l > h)
+        return 0;
     if (l == h && l == k)
-        return ST[i] + (lazy[i] * (h-l+1));
-        
-    push(i);
+        return ST[i] + lazy[i];                                 // reading only single element at a time
+
+    if (l != h)                                                 // internal node
+        push(i);
 
     int m = (h-l)/2 + l;
     if (k <= m)
@@ -76,21 +76,21 @@ int main() {
     A.assign(N, 0);
     ST.assign(4*N, 0);
     lazy.assign(4*N, 0);
-    marked.assign(4*N, false);
     for (int i = 0; i < N; i++)
         cin >> A[i];
 
     build_seg_tree(0, N-1, 0);
 
-    int type, l, h, x, k;
+    int type, l, h, k;
+    ll x;
     for (int i = 0; i < Q; i++) {
         cin >> type;
         if (type == 1) {
             cin >> l >> h >> x;
-            update(0, N-1, 0, l, h, x);
+            update(0, N-1, 0, l-1, h-1, x);
         } else {
             cin >> k;
-            int val = query(0, N-1, 0, k-1);
+            ll val = query(0, N-1, 0, k-1);
             cout << val << endl;
         }
     }
