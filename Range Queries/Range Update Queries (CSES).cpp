64c158b1f919b7b9eb6c566488759069fff04a19
Arpit Goyal <arpit.go4@gmail.com>
2024-08-26 15:11:19 +0530

    Refactors code for a problem,
    * Range Update Queries (Segment Tree, Range Add Updates, Lazy Propogation)

--- Range Queries/Range Update Queries (CSES).cpp
+++ Range Queries/Range Update Queries (CSES).cpp
@@ -8,11 +8,33 @@ using namespace std;
 // Time: O(N + QlogN)
 // Space: O(N)
  
+/**
+ * With lazy propogation model,
+ * 
+ * For `update` call, targetted node is marked and lazy update value
+ * is recorded. This makes the range updates `O(logN)`
+ * 
+ * For further `query` or `update` call, if a node has a marker on it
+ * new value is updated for it and lazy update is propogated further 
+ * down the tree.
+ * Above, optimization makes sure, we only propogate updates down the tree
+ * step-by-step and only where we need to propogate. Otherwise, with eager
+ * propogation time complexity will become `O(NlogN)`.
+ * 
+ * For `query` call specially, we need to check whether there is a lazy update
+ * pending on the node or not before reading it's value. 
+ * This is the case for leaf nodes where lazy updates can't be propogated further
+ * and there new values are updated in any of the previous queries.
+ */
+
 typedef long long ll;
 
 vector<ll> ST, A, lazy;
+vector<bool> marked;
 
 void build_seg_tree(int l, int h, int i) {
+    if (l > h)
+        return;
     if (l == h) {
         ST[i] = A[l];
         return;
@@ -22,42 +44,43 @@ void build_seg_tree(int l, int h, int i) {
     build_seg_tree(l, m, 2*i+1);
     build_seg_tree(m+1, h, 2*i+2);
 
-    ST[i] = ST[2*i+1] + ST[2*i+2];
+    ST[i] = ST[2*i+1] + ST[2*i+2];    
 }
 
-void push(int i) {
-    if (lazy[i] != 0) {
+void push(int l, int h, int i) {
+    if (marked[i]) {
         lazy[2*i+1] += lazy[i];
         lazy[2*i+2] += lazy[i];
+        marked[2*i+1] = marked[2*i+2] = true;
+
+        ST[i] += (lazy[i] * (h-l+1));
         lazy[i] = 0;
+        marked[i] = false;
     }
 }
 
 void update(int l, int h, int i, int p, int q, ll x) {
-    if (l > h || p > q)
+    if (p > q)
         return;
     if (l == p && h == q) {
+        marked[i] = true;
         lazy[i] += x;
         return;
     }
 
-    push(i);
-
+    push(l, h, i);
     int m = (h-l)/2 + l;
-    update(l, m, 2*i+1, p, min(m, q), x);
+    update(l, m, 2*i+1, p, min(q, m), x);
     update(m+1, h, 2*i+2, max(m+1, p), q, x);
 
     ST[i] = ST[2*i+1] + ST[2*i+2];
 }
 
 ll query(int l, int h, int i, int k) {
-    if (l > h)
-        return 0;
     if (l == h && l == k)
-        return ST[i] + lazy[i];                                 // reading only single element at a time
-
-    push(i);
-
+        return marked[i] ? ST[i] + lazy[i] : ST[i];
+    
+    push(l, h, i);
     int m = (h-l)/2 + l;
     if (k <= m)
         return query(l, m, 2*i+1, k);
@@ -74,6 +97,7 @@ int main() {
     A.assign(N, 0);
     ST.assign(4*N, 0);
     lazy.assign(4*N, 0);
+    marked.assign(4*N, false);
     for (int i = 0; i < N; i++)
         cin >> A[i];
 
