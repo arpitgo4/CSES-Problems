3c57b674f7505b61f41f740a08c3b4490f716522
Arpit Goyal <arpit.go4@gmail.com>
2024-08-26 20:11:55 +0530

    Improved logic for problem,
    * Range Update Queries (Segment Tree, Range Add Updates, Lazy Propogation)

--- Range Queries/Range Update Queries (CSES).cpp
+++ Range Queries/Range Update Queries (CSES).cpp
@@ -9,22 +9,38 @@ using namespace std;
 // Space: O(N)
  
 /**
- * With lazy propogation model,
+ * Lazy propogation model,
  * 
- * For `update` call, targetted node is marked and lazy update value
- * is recorded. This makes the range updates `O(logN)`
+ * For `update` call, target node is marked and new update value
+ * is also updated at the target node. 
+ * Since, update is not propogated at that time further down, 
+ * this makes the range updates `O(logN)`.
+ * But all the nodes (parents) above the target nodes are updated wit
+ * new value on the way back.
  * 
- * For further `query` or `update` call, if a node has a marker on it
- * new value is updated for it and lazy update is propogated further 
- * down the tree.
+ * For further `query` or `update` call, if a node has a marker lazy 
+ * update is propogated further down the tree and new update values 
+ * are also calculated for child nodes.
+ * 
  * Above, optimization makes sure, we only propogate updates down the tree
  * step-by-step and only where we need to propogate. Otherwise, with eager
  * propogation time complexity will become `O(NlogN)`.
  * 
- * For `query` call specially, we need to check whether there is a lazy update
- * pending on the node or not before reading it's value. 
- * This is the case for leaf nodes where lazy updates can't be propogated further
- * and there new values are updated in any of the previous queries.
+ * So to sum up, with lazy updates,
+ * 1. Nodes above the target range update node have new updates values all 
+ *    the time. Parent nodes can be queried and will provide correct results
+ *    for there respective segments.
+ * 2. Children nodes w.r.t. target node will have stale values till the time
+ *    either `query` or `update` reaches to them. This is why before accessing
+ *    the childrens' values we need to propogate the lazy update if there is.
+ * 
+ * `marked[i] == true`, marks the current node for lazy propogation before going
+ * further down in it's subtree and `lazy[i]` denotes the value that is to be 
+ * propogated.
+ * 
+ * With above in place, whenever I encounter any node in the segment tree, it's and 
+ * it's parents' values are updated with all the lazy updates, which may or may not 
+ * be the case of it's children.
  */
 
 typedef long long ll;
@@ -49,11 +65,17 @@ void build_seg_tree(int l, int h, int i) {
 
 void push(int l, int h, int i) {
     if (marked[i]) {
+        int m = (h-l)/2 + l;
+
+        // first update the new values in segment tree nodes with parent's lazy value
+        ST[2*i+1] += lazy[i] * (m-l+1);
+        ST[2*i+2] += lazy[i] * (h-m);
+
+        // then, set the parent's lazy value which will be further propogated to children nodes
         lazy[2*i+1] += lazy[i];
         lazy[2*i+2] += lazy[i];
         marked[2*i+1] = marked[2*i+2] = true;
 
-        ST[i] += (lazy[i] * (h-l+1));
         lazy[i] = 0;
         marked[i] = false;
     }
@@ -63,8 +85,25 @@ void update(int l, int h, int i, int p, int q, ll x) {
     if (p > q)
         return;
     if (l == p && h == q) {
-        marked[i] = true;
+        /**
+         * Here, if I update lazy[i] first and then use that to update ST[i],
+         * 
+         * lazy[i] += x;
+         * ST[i] += lazy[i] * (h-l+1);
+         * 
+         * This is incorrect. As, whatever we had in lazy[p] was updated in ST[i]
+         * and lazy[p] was propogated to ith node before reaching here, in the push(p) call.
+         * 
+         * Correct way is to,
+         * ST[i] += x * (h-l+1);
+         * lazy[i] += x;
+         * 
+         * Always update the current segment tree node with just the update value first and then
+         * put the new update in the lazy[i].
+         */
+        ST[i] += x * (h-l+1);
         lazy[i] += x;
+        marked[i] = true;
         return;
     }
 
@@ -78,7 +117,7 @@ void update(int l, int h, int i, int p, int q, ll x) {
 
 ll query(int l, int h, int i, int k) {
     if (l == h && l == k)
-        return marked[i] ? ST[i] + lazy[i] : ST[i];
+        return ST[i];
     
     push(l, h, i);
     int m = (h-l)/2 + l;
