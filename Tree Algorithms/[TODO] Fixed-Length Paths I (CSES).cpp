19d4fc48233f8e58e3f59537421174c516712cee
Arpit Goyal <arpit.go4@gmail.com>
2024-08-31 17:35:55 +0530

    Another try for problem,
    * Fixed-Length Paths I (Tree, Centroid Decomposition, DFS)

--- Tree Algorithms/[TODO] Fixed-Length Paths I (CSES).cpp
+++ Tree Algorithms/[TODO] Fixed-Length Paths I (CSES).cpp
@@ -3,191 +3,110 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <set>
-#include <algorithm>
  
 using namespace std;
  
 // Time: O(NlogN)
 // Space: O(N)
 
-// Time: O(N^2)
-// Space: O(N)
- 
 /**
- * Did Centroid Decomposition on given tree
- * Each path from root to leaf is of length O(logN)
+ * There is no concept of LCA without rooting the tree!!
+ * Rooting the tree defines the highest ancestor which further
+ * leads to subsequent ancestors in the tree.
  * 
- * Centroid Decomposition can also be used for finding
- * LCA in O(logN) time.
+ * Centroid tree is the rooted tree. Concept of LCA is applicable 
+ * on it. There are total O(N^2) pairs possible for N node tree, hence
+ * O(N^2) paths. With centroid tree, we can represent O(N^2) paths
+ * as a set of O(NlogN) paths, by breaking each path w.r.t. all ancestors.
+ * Any node can have atmost O(logN) ancestors.
  * 
- * Brute force solution,
- * 1. For every pair of node, check if distance(a, b) == K
+ * With general tree, our search space to find count of paths with K-edges
+ * is O(N^2) but with centroid tree search space reduces to O(NlogN)
  * 
- * Distance between any pair of nodes goes through there LCA.
- * distance(a, b) = distance(a, lca(a, b)) + distance(b, lca(a, b))
+ * Time complexity for building centroid tree, O(NlogN)
+ * 
+ * Total time complexity for the problem will be,
+ * Time: O(NlogN)
+ * Space: O(N)
 */
 
 typedef pair<int,int> edge;
  
-vector<set<int>> T;
-vector<vector<int>> CT;
-vector<edge> centroid_edges;
-vector<int> sz, s_time, e_time, depth, P;
-vector<int> dp;
+int N, K, path_count;
 
-int timer = 0;
-int counter = 0;
-int max_depth = 0;
+vector<vector<int>> T;
+vector<int> sz, dp;                             // dp[k] -> c, count of nodes at distance k from root of tree
+vector<bool> removed;
 
-void dfs4(int u, int p, int dep, bool contribute, int K) {
-    if (dep > K)
-        return;
-    max_depth = max(max_depth, dep);
-
-    if (contribute) {
-        counter += dp[K-dep];
-    } else {
-        dp[dep]++;
-    }
-
-    for (int v : CT[u])
-        if (v != p)
-            dfs4(v, u, dep+1, contribute, K);
-}
-
-void dfs3(int u, int p, int K, int N) {
-    max_depth = 0;
-    dp[0] = 1;
-    for (int v : CT[u]) {
-        if (v != p) {
-            dfs4(v, u, 1, true, K);
-            dfs4(v, u, 1, false, K);
-        }
-    }
-
-    for (int i = 0; i <= max_depth; i++)
-        dp[i] = 0;
-
-    for (int v : CT[u]) {
-        if (v != p) {
-            dfs3(v, u, K, N);
-        }
-    }
-}
-
-bool is_ancestor(int a, int b) {
-    return s_time[a] <= s_time[b] && e_time[b] <= e_time[a];
-}
-
-int distance(int a, int b, int N) {
-    if (is_ancestor(a, b) || is_ancestor(b, a)) 
-        return abs(depth[a] - depth[b]);
-
-    int u = a, v = b;
-    for (int i = 0; i < log2(N) && u != v; i++) {
-        if (depth[u] > depth[v])
-            u = P[u];
-        else if (depth[u] < depth[v])
-            v = P[v];
-        else {
-            u = P[u];
-            v = P[v];
-        }
-    }
-
-    return depth[a] + depth[b] - (2 * depth[u]);
-}
-
-void dfs2(int u, int p, int dep) {
-    s_time[u] = timer++;
-    depth[u] = dep;
-    P[u] = p;
-
-    for (int v : CT[u]) {
-        if (v != p) {
-            dfs2(v, u, dep+1);
-        }
-    }
-
-    e_time[u] = timer++;
-}
-
-void build_centroid_tree(vector<edge>& edges, int N) {
-    CT.assign(N+1, vector<int>());
-    for (edge& e : edges) {
-        int u = e.first, v = e.second;
-        CT[u].push_back(v);
-        CT[v].push_back(u);
-    }
-}
-
-int find_centroid(int u, int p, int N) {
-    for (int v : T[u]) {
-        if (v != p && sz[v] > N/2)
-            return find_centroid(v, u, N);
-    }
-
-    return u;
-}
-
-void dfs(int u, int p) {
+void find_subtree_sz(int u, int p) {
     sz[u] = 1;
     for (int v : T[u]) {
         if (v != p) {
-            dfs(v, u);
+            find_subtree_sz(v, u);
             sz[u] += sz[v];
         }
     }
 }
 
-void decompose(int u, int p_centroid, int N) {
-    sz.assign(N+1, 0);
-    dfs(u, -1);
-
-    int centroid = find_centroid(u, -1, N);
-    if (p_centroid != -1)
-        centroid_edges.push_back({ p_centroid, centroid });
-
-    vector<int> roots;
-    for (int v : T[centroid]) {
-        T[v].erase(centroid);
-        roots.push_back(v);
+int find_centroid(int u, int p) {
+    for (int v : T[u]) {
+        if (v != p && !removed[v] && sz[v] > N/2)                                // sz[v] > N/2
+            return find_centroid(v, u);
     }
-    T[centroid].clear();
 
-    for (int v : roots)
-        decompose(v, centroid, N);
+    return u;
 }
 
-void solve(vector<edge>& edges, int N, int K) {
-    T.assign(N+1, set<int>());
-    P.assign(N+1, -1);
-    depth.assign(N+1, 0);
-    s_time.assign(N+1, 0);
-    e_time.assign(N+1, 0);
-    dp.assign(N+1, 0);
-    for (edge& e : edges) {
-        int u = e.first, v = e.second;
-        T[u].insert(v);
-        T[v].insert(u);
+// dfs to count paths
+// edge_count, count of edges from centroid of tree
+void dfs(int u, int p, int edge_count) {
+    if (edge_count <= K)
+        path_count += dp[K-edge_count];
+
+    for (int v : T[u]) {
+        if (v != p && !removed[v]) {
+            dfs(v, u, edge_count+1);
+        }
     }
 
-    decompose(1, -1, N);
-    build_centroid_tree(centroid_edges, N);
+    dp[edge_count]++;
+}
 
-    dfs2(1, -1, 0);
+void decompose(int u, int p) {
+    int centroid = find_centroid(u, p);
 
-    dfs3(1, -1, K, N);
+    dp.assign(N+1, 0);
+    dp[0] = 1;
+    dfs(centroid, -1, 0);
 
-    cout << counter << endl;
+    removed[centroid] = true;
+    for (int v : T[centroid]) {
+        if (v != p && !removed[v]) {
+            decompose(v, centroid);
+        }
+    }
+}
+
+void solve(vector<edge>& edges) {
+    T.assign(N+1, vector<int>());
+    sz.assign(N+1, 0);
+    removed.assign(N+1, false);
+    path_count = 0;
+    for (auto& [u, v] : edges) {
+        T[u].push_back(v);
+        T[v].push_back(u);
+    }
+
+    find_subtree_sz(1, -1);
+    decompose(1, -1);
+    
+    cout << path_count << endl;
 }
  
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int N, K;
     cin >> N >> K;
 
     int u, v;
@@ -197,7 +116,7 @@ int main() {
         edges[i] = { u, v };
     }
 
-    solve(edges, N, K);
+    solve(edges);
     
     return 0;
 }
