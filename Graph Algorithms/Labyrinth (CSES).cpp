e1a39b0a2d64ac1e9f38edfb3f0bb4360bd177bc
arpitgo4 <arpit.go4@gmail.com>
2025-06-27 17:16:20 +0530

    Reimplemented solution for problem,
    * Labyrinth (Graphs, BFS, Shortest Path)

--- Graph Algorithms/Labyrinth (CSES).cpp
+++ Graph Algorithms/Labyrinth (CSES).cpp
@@ -1,103 +1,133 @@
 // Labyrinth (CSES)
-
+ 
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <tuple>
-
+#include <algorithm>
+ 
 using namespace std;
-
+ 
 // Time: O(N * M)
 // Space: O(N * M)
+ 
+using coord_t = pair<int,int>;
+using move_t = tuple<int,int,char>;
 
-// BFS traversal produces shortest path for non-weighted graphs
+vector<tuple<int,int,char>> moves {
+    { -1, 0, 'U' },              // up
+    { 1, 0, 'D' },               // down
+    { 0, -1, 'L' },              // left
+    { 0, 1, 'R' }                // right
+};
 
-vector<vector<int>> V;
-vector<vector<tuple<int,int,char>>> P;
-vector<char> path;
+void findShortestPath(
+    coord_t& src,
+    vector<vector<int>>& chart,
+    int chart_height,
+    int chart_width,
+    coord_t& dest,
+    vector<vector<int>>& vis,
+    vector<vector<move_t>>& parent
+) {
 
-void prepare_path(int i, int j) {
-    int p_i, p_j;
-    char move;
-    tie(p_i, p_j, move) = P[i][j];
+    queue<coord_t> coord_queue;
+    coord_queue.push(src);
+    auto& [ src_i, src_j ] = src;
+    vis[src_i][src_j] = 1;
+    
+    while (!coord_queue.empty()) {
+        auto [ curr_i, curr_j ] = coord_queue.front();
+        coord_queue.pop();
 
-    while (p_i != -1 && p_j != -1) {
-        path.push_back(move);
-        tie(p_i, p_j, move) = P[p_i][p_j];
+        if (curr_i == dest.first && curr_j == dest.second) {
+            return;
+        }
+
+        for (auto& [diff_i, diff_j, move_ch] : moves) {
+            int next_i = curr_i + diff_i;
+            int next_j = curr_j + diff_j;
+            if (next_i >= 0 && 
+                    next_i < chart_height && 
+                    next_j >= 0 && 
+                    next_j < chart_width && 
+                    chart[next_i][next_j] == 0 &&
+                    vis[next_i][next_j] == 0) {
+                        coord_queue.push({ next_i, next_j });
+                        parent[next_i][next_j] = { curr_i, curr_j, move_ch };
+                        vis[next_i][next_j] = 1;
+                    }
+        }
     }
 }
 
-bool bfs(vector<vector<char>>& G, int i, int j, int N, int M) {
-    queue<tuple<int,int,char,int,int>> q;
-    q.push({ i, j, 'N', -1, -1 });
+void solve(
+    vector<vector<int>>& chart,
+    int chart_height,
+    int chart_width,
+    coord_t& src,
+    coord_t& dest
+) {
+
+    vector<vector<int>> vis(chart_height+1, vector<int>(chart_width+1, 0));
+    vector<vector<move_t>> parent(chart_height+1, vector<move_t>(chart_width+1, {-1,-1,' '}));
     
-    while (!q.empty()) {
-        tuple<int,int,char,int,int> t = q.front();
-        q.pop();
+    findShortestPath(
+        src, 
+        chart, 
+        chart_height, 
+        chart_width, 
+        dest,
+        vis,
+        parent
+    );
 
-        int p_i, p_j, i, j;
-        char move;
-        tie(i, j, move, p_i, p_j) = t;
-
-        if (i < 0 || i == N || j < 0 || j == M || V[i][j] == 1 || G[i][j] == '#')
-            continue;
-
-        P[i][j] = { p_i, p_j, move };
-        V[i][j] = 1;
-
-        if (G[i][j] == 'B') {
-            prepare_path(i, j);
-            return true;
-        }
-
-        q.push({ i, j-1, 'L', i, j });
-        q.push({ i, j+1, 'R', i, j });
-        q.push({ i-1, j, 'U', i, j });
-        q.push({ i+1, j, 'D', i, j });
-    }
-
-    return false;
-}
-
-void solve(vector<vector<char>>& G, int N, int M) {
-    V.assign(N, vector<int>(M, 0));
-    P.assign(N, vector<tuple<int,int,char>>(M));
-
-    bool path_found = false;
-    int i = 0, j = 0;
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            if (G[i][j] == 'A') break;
-        }
-        if (G[i][j] == 'A') break;
-    }
-    
-    path_found = bfs(G, i, j, N, M);
-
-    if (!path_found) {
+    auto [ dest_i, dest_j ] = dest;
+    if (vis[dest_i][dest_j] == 0) {
         cout << "NO" << endl;
-        return;
+    } else {
+        cout << "YES" << endl;
+        vector<char> moves_taken;
+        coord_t curr = dest;
+        while (curr != src) {
+            auto& [ curr_i, curr_j ] = curr;
+            auto& [ par_i, par_j, move_ch ] = parent[curr_i][curr_j];
+            moves_taken.push_back(move_ch);
+            curr = { par_i, par_j };
+        }
+
+        cout << moves_taken.size() << endl;
+        reverse(moves_taken.begin(), moves_taken.end());
+        for (char move_ch : moves_taken) {
+            cout << move_ch;
+        }
+        cout << endl;
     }
-
-    cout << "YES" << endl;
-    cout << path.size() << endl;
-    for (int i = path.size()-1; i >= 0; i--)
-        cout << path[i];
 }
-
+ 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    
+    int chart_height, chart_width;
+    cin >> chart_height >> chart_width;
 
-    int N, M;
-    cin >> N >> M;
+    char ch;
+    coord_t src, dest;
+    vector<vector<int>> chart(chart_height, vector<int>(chart_width, 0));
+    for (int i = 0; i < chart_height; i++) {
+        for (int j = 0; j < chart_width; j++) {
+            cin >> ch;
+            if (ch == '#') {
+                chart[i][j] = 1;    
+            } else if (ch == 'A') {
+                src = { i, j };
+            } else if (ch == 'B') {
+                dest = { i, j };
+            }
+        }
+    }
 
-    vector<vector<char>> G(N, vector<char>(M));
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < M; j++)
-            cin >> G[i][j];
-
-    solve(G, N, M);
-
+    solve(chart, chart_height, chart_width, src, dest);
+    
     return 0;
 }
