0b5befb6e0ea4985aa03c4e38c64c4b04200ef2b
arpitgo4 <arpit.go4@gmail.com>
2025-06-27 18:30:20 +0530

    Reimplemented solution for problem,
    * Shortest Routes I (Graph, SSSP, Dijkstra)

--- Graph Algorithms/Shortest Routes I (CSES).cpp
+++ Graph Algorithms/Shortest Routes I (CSES).cpp
@@ -1,88 +1,103 @@
 // Shortest Routes I (CSES)
-
+ 
 #include <iostream>
 #include <vector>
-#include <climits>
 #include <queue>
-#include <tuple>
-
+#include <climits>
+ 
 using namespace std;
-
-// Time: O(V + ElogV)
+ 
+// Time: O(ElogV)
 // Space: O(V)
+ 
+using ll_t = long long;
+using graph_t = vector<vector<pair<int,ll_t>>>;
+using queue_state_t = pair<ll_t,int>;
 
-#define INF LLONG_MAX
+const ll_t INF = LLONG_MAX;
 
-typedef long long ll;
-typedef tuple<int,int,ll> edge;
-typedef pair<int,ll> g_edge;
-typedef pair<ll,int> state;
+class Flight {
+public:
+    int src_;
+    int dest_;
+    ll_t duration_;
 
-vector<vector<g_edge>> G;
+    Flight(int src, int dest, ll_t duration) {
+        src_ = src;
+        dest_ = dest;
+        duration_ = duration;
+    }
+};
 
-vector<int> vis;
-vector<ll> D;
 
-void dijkstra(int src) {
-    auto cmp = [&](const state& s1, const state& s2) {
+void findShortestPathFromSource(
+    int source,
+    graph_t& adj_list,
+    int city_cnt,
+    vector<ll_t>& dist
+) {
+    vector<int> vis(city_cnt+1, 0);
+
+    auto cmp = [&](auto& s1, auto& s2) {
         return s1.first > s2.first;
     };
-    priority_queue<state, vector<state>, decltype(cmp)> pq(cmp);            // default pq is max_heap, but with cmp it is min_heap
+    priority_queue<queue_state_t, vector<queue_state_t>, decltype(cmp)> city_queue(cmp);
+    
+    dist[source] = 0;
+    city_queue.push({ dist[source], source });
+    vis[source] = 1;
 
-    D[src] = 0;
-    pq.push({ D[src], src });
+    while (!city_queue.empty()) {
+        auto [ _, curr_city ] = city_queue.top();
+        city_queue.pop();
 
-    while (!pq.empty()) {
-        auto [ _, u ] = pq.top();
-        pq.pop();
-
-        if (vis[u] == 1)
+        if (vis[curr_city] == 2)
             continue;
-        vis[u] = 1;
 
-        for (g_edge& e : G[u]) {
-            auto& [ v, w ] = e;
-            if (D[v] > D[u] + w) {
-                D[v] = D[u] + w;
-                pq.push({ D[v], v });
+        for (auto& [ next_city, duration ] : adj_list[curr_city]) {
+            if (dist[next_city] > dist[curr_city] + duration) {
+                dist[next_city] = dist[curr_city] + duration;
+                city_queue.push({ dist[next_city], next_city });
+                vis[next_city] = 1;
             }
         }
+
+        vis[curr_city] = 2;
     }
-}
+};
 
-void solve(vector<edge>& edges, int E, int V) {
-    G.assign(V+1, vector<g_edge>());
-    vis.assign(V+1, 0);
-    D.assign(V+1, INF);
-
-    int u, v; ll w;
-    for (edge& e : edges) {
-        tie(u, v, w) = e;
-        G[u].push_back({ v, w });
+void solve(int city_cnt, vector<Flight>& flights, int flight_cnt) {
+    graph_t adj_list(city_cnt+1, vector<pair<int,ll_t>>());
+    for (Flight& flight : flights) {
+        adj_list[flight.src_]
+            .push_back({ flight.dest_, flight.duration_ });
     }
 
-    dijkstra(1);
+    vector<ll_t> dist(city_cnt+1, INF);
+    findShortestPathFromSource(1, adj_list, city_cnt, dist);
 
-    for (int u = 1; u <= V; u++)
-        cout << D[u] << " ";
+    for (int i = 1; i <= city_cnt; i++) {
+        cout << dist[i] << " ";
+    }
     cout << endl;
 }
-
+ 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    
+    int city_cnt, flight_cnt;
+    cin >> city_cnt >> flight_cnt;
 
-    int V, E;
-    cin >> V >> E;
-
-    int u, v; ll w;
-    vector<edge> edges(E);
-    for (int i = 0; i < E; i++) {
-        cin >> u >> v >> w;
-        edges[i] = { u, v, w };
+    int src, dest;
+    ll_t duration;
+    vector<Flight> flights;
+    for (int i = 0; i < flight_cnt; i++) {
+        cin >> src >> dest >> duration;
+        flights.emplace_back(src, dest, duration);
     }
 
-    solve(edges, E, V);
-
+    solve(city_cnt, flights, flight_cnt);
+    
     return 0;
 }
