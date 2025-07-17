a1821bd3f232fa74c259ae804c1f4f91e32c1258
Arpit Goyal <arpit.go4@gmail.com>
2025-07-17 19:14:59 +0530

    Reimplements solution to problem,
    * Building Teams (Graph, Bipartite Graph, DFS, Coloring)

--- Graph Algorithms/Building Teams (CSES).cpp
+++ Graph Algorithms/Building Teams (CSES).cpp
@@ -4,72 +4,97 @@
 #include <vector>
  
 using namespace std;
-
+ 
 // Time: O(V + E)
 // Space: O(V + E)
+ 
+class Friendship {
+public:
+    int pupil_a_;
+    int pupil_b_;
+    Friendship(int pupil_a, int pupil_b) {
+        pupil_a_ = pupil_a;
+        pupil_b_ = pupil_b;
+    }
+};
 
-typedef pair<int,int> edge;
+bool isBipartiteGraph(
+    int curr_pupil,
+    int curr_color,
+    vector<vector<int>>& adj_list,
+    vector<int>& color,
+    vector<int>& vis
+) {
+    vis[curr_pupil] = 1;
+    color[curr_pupil] = curr_color;
 
-vector<vector<int>> G;
-vector<int> vis, color;
+    for (int next_pupil : adj_list[curr_pupil]) {
+        if (vis[next_pupil] == 0) {
+            bool isBipartite = isBipartiteGraph(
+                next_pupil,
+                (curr_color + 1) % 2,
+                adj_list,
+                color,
+                vis
+            );
 
-bool is_bipartite_graph = true;
-
-void dfs(int u, int c) {
-    vis[u] = 1;
-    color[u] = c;
-
-    for (int v : G[u]) {
-        if (vis[v] == 0) {
-            dfs(v, (c+1) % 2);
-        } else {
-            if (color[v] == c)
-                is_bipartite_graph = false;
+            if (!isBipartite) {
+                return false;
+            }
+        } else {                                                // vis[i] = {1,2}, back-edge or cross-edge
+            if (color[curr_pupil] == color[next_pupil]) {
+                return false;
+            }
         }
     }
 
-    vis[u] = 2;
-}
+    vis[curr_pupil] = 2;
 
-void solve(vector<edge>& edges, int V, int E) {
-    G.assign(V+1, vector<int>());
-    vis.assign(V+1, 0);
-    color.assign(V+1, -1);
-    for (auto& [u, v] : edges) {
-        G[u].push_back(v);
-        G[v].push_back(u);
+    return true;
+};
+
+void solve(int pupil_cnt, vector<Friendship>& friendships, int friendship_cnt) {
+    vector<vector<int>> adj_list(pupil_cnt+1, vector<int>());
+    for (auto& [ pupil_a, pupil_b ] : friendships) {
+        adj_list[pupil_a].push_back(pupil_b);
+        adj_list[pupil_b].push_back(pupil_a);
     }
 
-    for (int u = 1; u <= V; u++) {
-        if (vis[u] == 0) {
-            dfs(u, 0);
+    vector<int> color(pupil_cnt+1, 0);
+    vector<int> vis(pupil_cnt+1, 0);
+
+    bool isBipartite = true;
+    for (int i = 1; i <= pupil_cnt && isBipartite; i++) {
+        if (vis[i] == 0) {
+            isBipartite = isBipartiteGraph(i, 0, adj_list, color, vis);    
         }
     }
 
-    if (!is_bipartite_graph) {
+    if (!isBipartite) {
         cout << "IMPOSSIBLE" << endl;
     } else {
-        for (int u = 1; u <= V; u++)
-            cout << (color[u] + 1) << " ";
+        for (int i = 1; i <= pupil_cnt; i++) {
+            cout << (color[i]+1) << " ";
+        }
         cout << endl;
-    }
+    };
 }
  
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int V, E;
-    cin >> V >> E;
+    int pupil_cnt, friendship_cnt;
+    cin >> pupil_cnt >> friendship_cnt;
 
-    int u, v;
-    vector<edge> edges(E);
-    for (int i = 0; i < E; i++) {
-        cin >> u >> v;
-        edges[i] = { u, v };    
+    int pupil_a, pupil_b;
+    vector<Friendship> friendships;
+    for (int i = 0; i < friendship_cnt; i++) {
+        cin >> pupil_a >> pupil_b;
+        friendships.emplace_back(pupil_a, pupil_b);
     }
 
-    solve(edges, V, E);
+    solve(pupil_cnt, friendships, friendship_cnt);
     
     return 0;
 }
