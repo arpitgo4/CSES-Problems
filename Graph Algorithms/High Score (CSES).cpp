4e4e9f8353e1756a6713f41aacc1eae14d8994c4
Arpit Goyal <arpit.go4@gmail.com>
2025-07-18 15:41:39 +0530

    Reimplements solution for problem,
    * High Score (Graphs, SSSP, Negative Cycle, Bellman-Ford's SSSP)

--- Graph Algorithms/High Score (CSES).cpp
+++ Graph Algorithms/High Score (CSES).cpp
@@ -4,13 +4,12 @@
 #include <vector>
 #include <climits>
 #include <cmath>
-#include <tuple>
 #include <bitset>
 
 using namespace std;
 
 // Time: O(VE + (V + E)) ~ O(VE)
-// Space: O(V)
+// Space: O(V + E)
 
 /**
  * 1. Bellman-ford will provide all the nodes involved in +ve cycles that are reachable 
@@ -25,109 +24,135 @@ using namespace std;
  * return the path weight (i.e. max score)
 */
 
-#define INF LLONG_MAX
+using ll_t = long long;
 
-typedef long long ll;
-typedef tuple<int,int,ll> edge;
-typedef pair<int,ll> graph_edge;
+const ll_t INF = LLONG_MAX;
+const int MAX_ROOM_CNT = 2500;
 
-const int MAX_V = 2500;
+class Tunnel {
+public:
+    int room_1_;
+    int room_2_;
+    ll_t score_;
+    Tunnel(int room_1, int room_2, ll_t score) {
+        room_1_ = room_1;
+        room_2_ = room_2;
+        score_ = score;
+    }
+};
 
-vector<vector<graph_edge>> GT;
-vector<ll> dist;
-vector<graph_edge> P;                               // shortest-path tree
-bitset<MAX_V+1> src_mask, dest_mask;
-vector<int> vis;
+void findSingleSourceShortestPath(
+    int source,
+    int room_cnt,
+    vector<Tunnel>& tunnels,
+    vector<ll_t>& dist
+) {
+    for (Tunnel& tunnel : tunnels) {
+        tunnel.score_ = -tunnel.score_;
+    }
 
-bool bellman_ford_sssp(int src, int V, vector<edge>& edges) {
-    dist[src] = 0;
-
-    int u, v; ll w;
-    for (int i = 0; i < V-1; i++) {
-        for (edge& e : edges) {
-            tie(u,v,w) = e;
-            w = -w;
-            if (dist[u] != INF && dist[v] > dist[u] + w) {
-                dist[v] = dist[u] + w;
-                P[v] = { u, -w };
+    dist[source] = 0;
+    for (int i = 0; i < room_cnt-1; i++) {
+        for (auto& [ room_1, room_2, score ] : tunnels) {
+            if (dist[room_1] != INF && dist[room_2] > dist[room_1] + score) {
+                dist[room_2] = dist[room_1] + score;
             }
         }
     }
+}
 
+bool checkNegativeCycle(
+    int room_cnt,
+    vector<Tunnel>& tunnels,
+    vector<ll_t>& dist,
+    bitset<MAX_ROOM_CNT+1>& nodes_in_neg_cycle
+) {
     bool any = false;
-    for (edge& e : edges) {
-        tie(u,v,w) = e;
-        w = -w;
-        if (dist[u] != INF && dist[v] > dist[u] + w) {
-            dist[v] = dist[u] + w;
-            P[v] = { u, -w };
-            src_mask.set(v);
+    for (auto& [ room_1, room_2, score ] : tunnels) {
+        if (dist[room_1] != INF && dist[room_2] > dist[room_1] + score) {
             any = true;
+            nodes_in_neg_cycle.set(room_2);
         }
     }
 
     return any;
 }
 
-void dfs(int u) {
-    vis[u] = 1;
+void findReachableRooms(
+    int curr_room,
+    vector<vector<int>>& adj_list,
+    bitset<MAX_ROOM_CNT+1>& nodes_reachable_from_dest
+) {
+    nodes_reachable_from_dest.set(curr_room);
 
-    for (graph_edge& e : GT[u]) {
-        int v = e.first;
-        if (vis[v] == 0)
-            dfs(v);
-
-        dest_mask.set(v);
+    for (int next_room : adj_list[curr_room]) {
+        if (!nodes_reachable_from_dest.test(next_room)) {
+            findReachableRooms(
+                next_room,
+                adj_list,
+                nodes_reachable_from_dest
+            );
+        }
     }
-
-    vis[u] = 2;
 }
 
-void solve(vector<edge>& edges, int E, int V) {
-    GT.assign(V+1, vector<graph_edge>());
-    dist.assign(V+1, INF);
-    P.assign(V+1, { -1, INF });
-    vis.assign(V+1, 0);
- 
-    int u, v; ll w;
-    for (edge& e : edges) {
-        tie(u, v, w) = e;
-        GT[v].push_back({ u, w });
-    }
+void solve(const int room_cnt, vector<Tunnel>& tunnels, int tunnel_cnt) {
+    vector<ll_t> dist(room_cnt+1, INF);
+    findSingleSourceShortestPath(
+        1,
+        room_cnt,
+        tunnels,
+        dist
+    );
 
-    bellman_ford_sssp(1, V, edges);
-    dfs(V);
+    bitset<MAX_ROOM_CNT+1> nodes_in_neg_cycle;
+    bool neg_cyc_found = checkNegativeCycle(
+        room_cnt,
+        tunnels,
+        dist,
+        nodes_in_neg_cycle
+    );
 
-    if ((src_mask & dest_mask) != 0) {
-        cout << -1 << endl;
+    if (!neg_cyc_found) {
+        cout << (-dist[room_cnt]) << endl;
         return;
     }
 
-    ll max_score = 0;
-    graph_edge& curr = P[V];
-    while (curr.first != -1) {
-        max_score += curr.second;
-        curr = P[curr.first];
+    vector<vector<int>> adj_list(room_cnt+1, vector<int>());
+    for (auto& [ room_1, room_2, _ ] : tunnels) {
+        adj_list[room_2].push_back(room_1);
     }
+    
+    bitset<MAX_ROOM_CNT+1> nodes_reachable_from_dest;
+    findReachableRooms(
+        room_cnt,
+        adj_list,
+        nodes_reachable_from_dest
+    );
 
-    cout << max_score << endl;
+    if ((nodes_in_neg_cycle & nodes_reachable_from_dest) == 0) {
+        cout << (-dist[room_cnt]) << endl;
+    } else {
+        cout << -1 << endl;
+    }
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int V, E;
-    cin >> V >> E;
+    int room_cnt, tunnel_cnt;
+    cin >> room_cnt >> tunnel_cnt;
 
-    vector<edge> edges(E);
-    int u, v; ll w;
-    for (int i = 0; i < E; i++) {
-        cin >> u >> v >> w;
-        edges[i] = { u, v, w };
+    vector<Tunnel> tunnels;
+    int room_1, room_2;
+    ll_t tunnel_score;
+    for (int i = 0; i < tunnel_cnt; i++) {
+        cin >> room_1 >> room_2 >> tunnel_score;
+        tunnels.emplace_back(room_1, room_2, tunnel_score);
     }
 
-    solve(edges, E, V);
+    solve(room_cnt, tunnels, tunnel_cnt);
 
     return 0;
 }
