6047dd42aef8f1f257fa037d53ee88abf104b031
Arpit Goyal <arpit.go4@gmail.com>
2025-07-22 17:27:21 +0530

    Reimplements the solution for problem,
    * Cycle Finding (Graphs, Negative Cycle Finding, Bellman-Ford SSSP)

--- Graph Algorithms/Cycle Finding (CSES).cpp
+++ Graph Algorithms/Cycle Finding (CSES).cpp
@@ -2,84 +2,122 @@
  
 #include <iostream>
 #include <vector>
-#include <tuple>
-#include <climits>
+#include <numeric>
 #include <algorithm>
  
 using namespace std;
-
+ 
 // Time: O(VE)
-// Space: O(V)
+// Space: O(V + E)
+ 
+using ll_t = long long;
 
-#define INF LLONG_MAX
+class Edge {
+public:
+    int node_1_;
+    int node_2_;
+    ll_t weight_;
+    Edge(int node_1, int node_2, ll_t weight) {
+        node_1_ = node_1;
+        node_2_ = node_2;
+        weight_ = weight;
+    }
+};
 
-typedef long long ll;
-typedef tuple<int,int,ll> edge;
-
-vector<ll> dist;
-vector<int> P;                                          // shortest-path (SP) tree
-
-int bellman_ford_sssp(int src, int V, vector<edge>& edges) {
-    int cycle_vertex = 0;
-    int u, v; ll w;
-    for (int i = 0; i < V && cycle_vertex != -1; i++) {                 // iterating V times instead of (V-1), to also check for presence of negative cycle
-        cycle_vertex = -1;
-
-        for (edge& e : edges) {
-            tie(u,v,w) = e;
-            if (dist[u] != INF && dist[v] > dist[u] + w) {
-                dist[v] = dist[u] + w;
-                P[v] = u;             // Vth iteration, will form a cycle in the SP tree. Root of the tree will have parent as the lower-level child
-                cycle_vertex = v;
+bool checkNegativeCycle(
+    int node_cnt,
+    vector<Edge>& edges,
+    vector<ll_t>& dist,
+    vector<bool>& node_in_neg_cycle,
+    vector<int>& parent
+) {
+    for (int i = 0; i < node_cnt-1; i++) {
+        for (auto& [ node_1, node_2, weight ] : edges) {
+            if (dist[node_2] > dist[node_1] + weight) {
+                dist[node_2] = dist[node_1] + weight;
+                parent[node_2] = node_1;
             }
         }
     }
 
-    return cycle_vertex;
+    bool neg_cycle_found = false;
+    for (auto& [ node_1, node_2, weight ] : edges) {
+        if (dist[node_2] > dist[node_1] + weight) {
+            neg_cycle_found = true;
+            dist[node_2] = dist[node_1] + weight;
+            parent[node_2] = node_1;
+            node_in_neg_cycle[node_2] = true;
+        }
+    }
+
+    return neg_cycle_found;
 }
 
-void solve(vector<edge>& edges, int V, int E) {
-    dist.assign(V+1, 0);                            // looking for shortest path simultaneously from all vertices. This helps in finding if there is neg-cycle reachable from any vertex, in case graph is disconnected. Otherwise, looking for a single source would have been fine!
-    P.assign(V+1, -1);
+void solve(int node_cnt, vector<Edge>& edges, int edge_cnt) {
+    vector<ll_t> dist(node_cnt+1, 0);
+    vector<int> parent(node_cnt+1, -1);
+    iota(parent.begin(), parent.end(), 0);
+    vector<bool> node_in_neg_cycle(node_cnt+1, false);
 
-    int cycle_vertex = bellman_ford_sssp(1, V, edges);
-    if (cycle_vertex == -1) {
+    bool negativeCycleFound = checkNegativeCycle(
+        node_cnt,
+        edges,
+        dist,
+        node_in_neg_cycle,
+        parent
+    );
+
+    if (!negativeCycleFound) {
         cout << "NO" << endl;
         return;
-    } else cout << "YES" << endl;
+    }
 
-    for (int i = 0; i < V-1; i++)
-        cycle_vertex = P[cycle_vertex];             // if cycle_vertex is a vertex that is reachable from -ve cycle, this loop will take us back into the -ve cycle!
+    cout << "YES" << endl;
+    int curr_node = 0;
+    for (int i = 1; i <= node_cnt; i++) {
+        if (node_in_neg_cycle[i]) {
+            curr_node = i;
+            break;
+        }
+    }
 
-    vector<int> cycle_path;
-    int curr = cycle_vertex;
+    // with this loop, we can be sure that
+    // we will enter the cycle
+    for (int i = 0; i < node_cnt-1; i++) {
+        curr_node = parent[curr_node];
+    }
+
+    vector<int> cycle;
+    int cycle_start_node = curr_node;
     do {
-        cycle_path.push_back(curr);
-        curr = P[curr];
-    } while (curr != cycle_vertex);
+        cycle.push_back(curr_node);
+        curr_node = parent[curr_node];
+    } while(curr_node != cycle_start_node);
+    cycle.push_back(cycle_start_node);
 
-    cycle_path.push_back(curr);
-    reverse(cycle_path.begin(), cycle_path.end());
-
-    for (int u : cycle_path)
-        cout << u << " ";
+    reverse(cycle.begin(), cycle.end());
+    for (int node : cycle) {
+        cout << node << " ";
+    }
+    cout << endl;
 }
  
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int V, E;
-    cin >> V >> E;
+    int node_cnt, edge_cnt;
+    cin >> node_cnt >> edge_cnt;
 
-    int u, v; ll w;
-    vector<tuple<int,int,ll>> edges(E);
-    for (int i = 0; i < E; i++) {
-        cin >> u >> v >> w;
-        edges[i] = { u, v, w };    
+    int node_1, node_2;
+    ll_t weight;
+    vector<Edge> edges;
+    for (int i = 0; i < edge_cnt; i++) {
+        cin >> node_1 >> node_2 >> weight;
+        edges.emplace_back(node_1, node_2, weight);
     }
 
-    solve(edges, V, E);   
+    solve(node_cnt, edges, edge_cnt);
     
     return 0;
 }
