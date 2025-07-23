866ae97a76c217ad36d60f6fb0adbd5602646640
Arpit Goyal <arpit.go4@gmail.com>
2025-07-23 14:08:11 +0530

    Reimplements solution for problem,
    * Planets and Kingdoms (Graphs, Tarjans SCC algorithm)

--- Graph Algorithms/Planets and Kingdoms (CSES).cpp
+++ Graph Algorithms/Planets and Kingdoms (CSES).cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <numeric>
 #include <unordered_map>
  
 using namespace std;
@@ -10,67 +11,132 @@ using namespace std;
 // Time: O(V + E)
 // Space: O(V + E)
  
-typedef pair<int,int> edge;
+using graph_t = vector<vector<int>>;
 
-vector<vector<int>> G;
-vector<int> vis, low_link, s_time;
-vector<int> scc_roots, P;          // P, path-compressed parent-pointer tree
-vector<bool> on_stack;
-stack<int> st;
-
-int timer = 0;
-
-void dfs(int u) {
-    s_time[u] = low_link[u] = timer++;
-    st.push(u);
-    on_stack[u] = true;
-    vis[u] = 1;
-
-    for (int v : G[u]) {
-        if (vis[v] == 0) {
-            dfs(v);
-            low_link[u] = min(low_link[u], low_link[v]);
-        } else if (on_stack[v])
-            low_link[u] = min(low_link[u], s_time[v]);
+class Teleporter {
+public:
+    int src_planet_;
+    int dest_planet_;
+    Teleporter(int src_planet, int dest_planet) {
+        src_planet_ = src_planet;
+        dest_planet_ = dest_planet;
     }
+};
 
-    if (s_time[u] == low_link[u]) {
-        scc_roots.push_back(u);
+void findStronglyConnectedComponents(
+    int curr_planet,
+    graph_t& adj_list,
+    int& time,
+    stack<int>& planet_stack,
+    vector<bool>& on_planet_stack,
+    vector<int>& parent,
+    vector<int>& low_link,
+    vector<int>& start_time,
+    vector<int>& vis
+) {
 
-        int v;
+    vis[curr_planet] = 1;
+    start_time[curr_planet] = time++;
+    low_link[curr_planet] = start_time[curr_planet];
+    planet_stack.push(curr_planet);
+    on_planet_stack[curr_planet] = true;
+
+    for (int next_planet : adj_list[curr_planet]) {
+        if (vis[next_planet] == 0) {
+            findStronglyConnectedComponents(
+                next_planet,
+                adj_list,
+                time,
+                planet_stack,
+                on_planet_stack,
+                parent,
+                low_link,
+                start_time,
+                vis
+            );
+
+            low_link[curr_planet] = min(
+                low_link[curr_planet],
+                low_link[next_planet]
+            );
+        } else if (on_planet_stack[next_planet]) {
+            low_link[curr_planet] = min(
+                low_link[curr_planet],
+                start_time[next_planet]
+            );
+        }
+    }
+    
+    if (low_link[curr_planet] == start_time[curr_planet]) {
+        int planet_on_stack;
         do {
-            v = st.top();
-            st.pop();
-            on_stack[v] = false;
-            P[v] = u;
-        } while (v != u);
+            planet_on_stack = planet_stack.top();
+            planet_stack.pop();
+            on_planet_stack[planet_on_stack] = false;
+            parent[planet_on_stack] = curr_planet;
+        } while (planet_on_stack != curr_planet);
     }
 }
 
-void solve(vector<edge>& edges, int V, int E) {
-    G.assign(V+1, vector<int>());
-    vis.assign(V+1, 0);
-    s_time.assign(V+1, -1);
-    low_link.assign(V+1, -1);
-    P.assign(V+1, -1);
-    on_stack.assign(V+1, false);
-    for (edge& e : edges) {
-        int u = e.first, v = e.second;
-        G[u].push_back(v);
+void solve(
+    int planet_cnt,
+    vector<Teleporter>& teleporters,
+    int teleporter_cnt
+) {
+    graph_t adj_list(planet_cnt+1, vector<int>());
+    for (auto& [ src_planet, dest_planet ] : teleporters) {
+        adj_list[src_planet].push_back(dest_planet);
     }
 
-    for (int u = 1; u <= V; u++)
-        if (vis[u] == 0)
-            dfs(u);
+    int time = 0;
+    vector<int> vis(planet_cnt+1, 0);
+    vector<int> low_link(planet_cnt+1, -1);
+    vector<int> start_time(planet_cnt+1, 0);
+    vector<int> parent(planet_cnt+1, -1);
+    vector<bool> on_planet_stack(planet_cnt+1, false);
+    stack<int> planet_stack;
+    iota(parent.begin(), parent.end(), 0);
 
-    int idx = 1;
-    unordered_map<int,int> labels;
-    for (int r : scc_roots)
-        labels[r] = idx++;
+    for (int curr_planet = 1; 
+        curr_planet <= planet_cnt; 
+        curr_planet++) {
+            if (vis[curr_planet] == 0) {
+                findStronglyConnectedComponents(
+                    curr_planet,
+                    adj_list,
+                    time,
+                    planet_stack,
+                    on_planet_stack,
+                    parent,
+                    low_link,
+                    start_time,
+                    vis
+                );
+            }
+    }
 
-    cout << scc_roots.size() << endl;
-    for (int u = 1; u <= V; u++)
-        cout << labels[P[u]] << " ";
+    int label_counter = 0;
+    unordered_map<int,int> parent_label_mapping;
+    for (int curr_planet = 1; 
+        curr_planet <= planet_cnt; 
+        curr_planet++) {
+
+        int parent_planet = parent[curr_planet];
+        if (parent_label_mapping.find(parent_planet) == 
+                parent_label_mapping.end()) {
+
+            parent_label_mapping[parent_planet] = ++label_counter;
+        }
+    }
+
+    cout << parent_label_mapping.size() << endl;
+    for (int curr_planet = 1; 
+        curr_planet <= planet_cnt; 
+        curr_planet++) {
+
+        int parent_planet = parent[curr_planet];
+        cout << parent_label_mapping[parent_planet] << " ";    
+    }
     cout << endl;
 }
  
@@ -78,17 +144,20 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int V, E;
-    cin >> V >> E;
+    int planet_cnt, teleporter_cnt;
+    cin >> planet_cnt >> teleporter_cnt;
 
-    int u, v;
-    vector<edge> edges(E);
-    for (int i = 0; i < E; i++) {
-        cin >> u >> v;
-        edges[i] = { u, v };
+    int src_planet, dest_planet;
+    vector<Teleporter> teleporters;
+    for (int i = 0; i < teleporter_cnt; i++) {
+        cin >> src_planet >> dest_planet;
+        teleporters.emplace_back(
+            src_planet,
+            dest_planet
+        );
     }
 
-    solve(edges, V, E);
+    solve(planet_cnt, teleporters, teleporter_cnt);
     
     return 0;
 }
