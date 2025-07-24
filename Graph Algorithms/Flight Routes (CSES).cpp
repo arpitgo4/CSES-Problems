4c7beb16fe1cbbb26e8ffe00374e8f41f8d329b1
Arpit Goyal <arpit.go4@gmail.com>
2025-07-24 21:46:51 +0530

    Reimplemented solution for problem,
    * Flight Routes (Graphs, Dijkstra SSSP)

--- Graph Algorithms/Flight Routes (CSES).cpp
+++ Graph Algorithms/Flight Routes (CSES).cpp
@@ -4,73 +4,121 @@
 #include <vector>
 #include <climits>
 #include <queue>
-#include <tuple>
  
 using namespace std;
-using ll = long long;
+ 
+// Time: O(K * ElogE)
+// Space: O(E)
+ 
+using ll_t = long long;
+using graph_edge_t = pair<ll_t,int>;
+using graph_t = vector<vector<graph_edge_t>>;
+using queue_state_t = pair<ll_t,int>;
 
-// Time: O(V + KElogV) ~ O(KElogV)
-// Space: O(V)
+const ll_t INF = LLONG_MAX;
 
-vector<vector<pair<int,ll>>> G;
-vector<int> counter;
+class Flight {
+public:
+    int src_city_;
+    int dest_city_;
+    ll_t price_;
+    Flight(int src_city, int dest_city, ll_t price) {
+        src_city_ = src_city;
+        dest_city_ = dest_city;
+        price_ = price;
+    }
+};
 
-void dijkstra(int src, int K, int dest) {
-    auto comp = [&](auto& p1, auto& p2) {
-        return p1.first > p2.first;
+void findShortestRoutePrices(
+    int src_city,
+    int dest_city,
+    graph_t& adj_list,
+    int city_cnt,
+    int shortest_route_cnt,
+    vector<ll_t>& shortest_prices
+) {
+    auto cmp = [&](auto& s1, auto& s2) {
+        return s1.first > s2.first;
     };
-    priority_queue<pair<ll,int>, vector<pair<ll,int>>, decltype(comp)> pq(comp);  // min-heap
+    priority_queue<
+        queue_state_t,
+        vector<queue_state_t>,
+        decltype(cmp)
+    > city_queue(cmp);
 
-    pq.push({ 0, src });
+    vector<ll_t> dist(city_cnt+1, INF);
+    vector<int> dist_counter(city_cnt+1, 0);
+    dist[src_city] = 0;
+    dist_counter[src_city] = 0;
+    city_queue.emplace(dist[src_city], src_city);
 
-    while (!pq.empty() && counter[dest] < K) {
-        auto p = pq.top();
-        pq.pop();
+    while (!city_queue.empty()) {
+        auto [ curr_dist, curr_city ] = city_queue.top();
+        city_queue.pop();
 
-        int u = p.second;
-        ll d = p.first;
-
-        counter[u]++;
-        if (u == dest)
-            cout << d << " ";
-
-        if (counter[u] <= K) {
-            for (auto& e : G[u]) {
-                int v = e.first; ll w = e.second;
-                pq.push({ d+w, v });
+        dist_counter[curr_city]++;
+        if (dist_counter[curr_city] > shortest_route_cnt) {
+            continue;
+        }
+        if (curr_city == dest_city) {
+            shortest_prices.emplace_back(curr_dist);
+        }
+        
+        for (auto& [ next_city, flight_price ] : adj_list[curr_city]) {
+            if (dist[next_city] > dist[curr_city] + flight_price) {
+                dist[next_city] = dist[curr_city] + flight_price;
+                city_queue.emplace(dist[next_city], next_city);
+            } else {
+                ll_t next_city_price = curr_dist + flight_price;
+                city_queue.emplace(next_city_price, next_city);
             }
         }
     }
 }
 
-void solve(int V, int E, int K, vector<tuple<int,int,int>>& edges) {
-    G.assign(V+1, vector<pair<int,ll>>());
-    counter.assign(V+1, 0);
-
-    for (tuple<int,int,int>& t : edges) {
-        int u, v; ll w;
-        tie(u,v,w) = t;
-        G[u].push_back({ v, w });
+void solve(
+    int city_cnt,
+    int shortest_route_cnt,
+    vector<Flight>& flights,
+    int flight_cnt
+) {
+    graph_t adj_list(city_cnt+1, vector<graph_edge_t>());
+    for (auto& [ src_city, dest_city, price ] : flights) {
+        adj_list[src_city].emplace_back(dest_city, price);
     }
 
-    dijkstra(1, K, V);
+    vector<ll_t> shortest_prices;
+    findShortestRoutePrices(
+        1,
+        city_cnt,
+        adj_list,
+        city_cnt,
+        shortest_route_cnt,
+        shortest_prices
+    );
+
+    for (ll_t price : shortest_prices) {
+        cout << price << " ";
+    }
+    cout << endl;
 }
  
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    int V, E, K;
-    cin >> V >> E >> K;
 
-    vector<tuple<int,int,int>> edges(E);
-    int u, v, w;
-    for (int i = 0; i < E; i++) {
-        cin >> u >> v >> w;
-        edges[i] = { u, v, w };
+    int city_cnt, flight_cnt, shortest_route_cnt;
+    cin >> city_cnt >> flight_cnt >> shortest_route_cnt;
+
+    int src_city, dest_city;
+    ll_t price;
+    vector<Flight> flights;
+    for (int i = 0; i < flight_cnt; i++) {
+        cin >> src_city >> dest_city >> price;
+        flights.emplace_back(src_city, dest_city, price);
     }
 
-    solve(V, E, K, edges);   
+    solve(city_cnt, shortest_route_cnt, flights, flight_cnt);
     
     return 0;
 }
--- Graph Algorithms/Mail Delivery (CSES).cpp
+++ Graph Algorithms/Mail Delivery (CSES).cpp
@@ -2,20 +2,53 @@
  
 #include <iostream>
 #include <vector>
+#include <stack>
  
 using namespace std;
  
 // Euler's Tour
 
-void solve() {
- 
+using graph_t = vector<vector<int>>;
+
+class Street {
+public:
+    int crossing_1_;
+    int crossing_2_;
+    Street(int crossing_1, int crossing_2) {
+        crossing_1_ = crossing_1;
+        crossing_2_ = crossing_2;
+    }
+};
+
+void solve(
+    int crossing_cnt,
+    vector<Street>& streets,
+    int street_cnt
+) {
+    graph_t adj_list(crossing_cnt+1, vector<int>());
+    for (auto& [ crossing_1, crossing_2 ] : streets) {
+        adj_list[crossing_1].push_back(crossing_2);
+        adj_list[crossing_2].push_back(crossing_1);
+    }
+
+    
 }
  
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    
+    int crossing_cnt, street_cnt;
+    cin >> crossing_cnt >> street_cnt;
+
+    int crossing_1, crossing_2;
+    vector<Street> streets;
+    for (int i = 0; i < street_cnt; i++) {
+        cin >> crossing_1 >> crossing_2;
+        streets.emplace_back(crossing_1, crossing_2);
+    }
+
+    solve(crossing_cnt, streets, street_cnt);
     
     return 0;
 }
