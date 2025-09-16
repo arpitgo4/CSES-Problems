6747029d2cdb5db346a966e358f90565dcc6b983
Arpit Goyal <arpit.go4@gmail.com>
2025-09-17 01:03:07 +0400

    Adds solution to problem,
    * Money Sums (DFS, Subsets, Combinations, DP)

--- Dynamic Programming/Money Sums (CSES).cpp
+++ Dynamic Programming/Money Sums (CSES).cpp
@@ -2,25 +2,92 @@
 
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
-void solve(vector<int>& A, int N) {
+// Time: O(N * K)
+// Space: O(N * K)
+
+void allCoinCombinations(
+    int curr_coin_idx,
+    int curr_sum,
+    vector<int>& coins,
+    int coin_cnt,
+    vector<vector<int>>& vis,
+    set<int>& sums_set
+) {
+    if (curr_coin_idx == coin_cnt) {
+        sums_set.insert(curr_sum);
+        return;
+    }
+    if (vis[curr_coin_idx][curr_sum] != 0) {
+        return;
+    }
+
+    vis[curr_coin_idx][curr_sum] = 1;
+
+    // include curr_coin
+    allCoinCombinations(
+        curr_coin_idx+1,
+        curr_sum + coins[curr_coin_idx],
+        coins,
+        coin_cnt,
+        vis,
+        sums_set
+    );
     
+    // exclude curr_coin
+    allCoinCombinations(
+        curr_coin_idx+1,
+        curr_sum,
+        coins,
+        coin_cnt,
+        vis,
+        sums_set
+    );
+    
+    vis[curr_coin_idx][curr_sum] = 2;
+}
+
+void solve(
+    vector<int>& coins,
+    int coin_cnt
+) {
+    const int MAX_SUM = 1e5;
+    vector<vector<int>> vis(coin_cnt+1, vector<int>(MAX_SUM+1, 0));
+    set<int> sums_set;
+    allCoinCombinations(
+        0,
+        0,
+        coins,
+        coin_cnt,
+        vis,
+        sums_set
+    );
+
+    cout << (sums_set.size()-1) << endl;
+    for (int sum : sums_set) {
+        if (sum > 0) {
+            cout << sum << " ";
+        }
+    }
+    cout << endl;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int N;
-    cin >> N;
+    int coin_cnt;
+    cin >> coin_cnt;
 
-    vector<int> A(N);
-    for (int i = 0; i < N; i++)
-        cin >> A[i];
+    vector<int> coins(coin_cnt);
+    for (int i = 0; i < coin_cnt; i++) {
+        cin >> coins[i];
+    }
 
-    solve(A, N);
+    solve(coins, coin_cnt);
 
     return 0;
 }
