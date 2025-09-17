242ec59c82982e9e5e5bd84e31d04e94c6f1c94e
Arpit Goyal <arpit.go4@gmail.com>
2025-09-18 01:21:26 +0400

    Adds solution to problem,
    * Elevator Rides (Permutation, Combinatorics, Backtracking, Brute-force)

--- Dynamic Programming/Elevator Rides (CSES).cpp
+++ Dynamic Programming/Elevator Rides (CSES).cpp
@@ -1,50 +1,84 @@
 // Elevator Rides (CSES)
-
+ 
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
+#include <climits>
+ 
 using namespace std;
-
-/**
- * Try out all possible permutations of people and calculate number of elevator rides for each one.
- * Return the minimum elevator ride count among all possible permutations.
- * 
- * Number of permutations = N!
- * Time to calculate no. of rides = O(N)
-*/
-
-// Time: O(N * N!) ~ O(N!)
+ 
+// Time: O(N!)
 // Space: O(N)
+ 
+using ll_t = long long;
 
-// Time: O(2^N)
-// Space: O(2^N)
+const int INF = INT_MAX;
 
-vector<vector<int>> cache;
+int countElevatorRides(
+    vector<int>& people_weights,
+    int people_cnt,
+    int max_elev_weight
+) {
+    int ride_cnt = 1;
+    ll_t curr_ride_weight = 0;
+    int i = 0;
+    while (i < people_cnt) {
+        if ((curr_ride_weight + people_weights[i]) <= max_elev_weight) {
+            curr_ride_weight += people_weights[i];
+        } else {
+            curr_ride_weight = people_weights[i];
+            ride_cnt++;
+        }
+        i++;
+    }
 
-int dfs(int i, int mask) {
-
+    return ride_cnt;
 }
 
-void solve(vector<int>& A, int N, int MAX_W) {
-    cache.assign();
+void solve(
+    vector<int>& people_weights,
+    int people_cnt,
+    int max_elev_weight
+) {
+    sort(people_weights.begin(), people_weights.end());
 
-    int count = dfs(0, (1 << ??) - 1);
+    int min_elev_rides = INF;
+    do {
+        int rides_cnt = countElevatorRides(
+            people_weights,
+            people_cnt,
+            max_elev_weight
+        );
 
-    cout << count;
+        min_elev_rides = min(
+            min_elev_rides,
+            rides_cnt
+        );
+    } while(next_permutation(
+        people_weights.begin(), people_weights.end()
+    ));
+
+    cout << min_elev_rides << endl;
 }
-
+ 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    
+    int people_cnt, max_elev_weight;
+    cin >> people_cnt 
+        >> max_elev_weight;
 
-    int N, MAX_W;
-    cin >> N >> MAX_W;
+    vector<int> people_weights(people_cnt);
+    for (int i = 0; i < people_cnt; i++) {
+        cin >> people_weights[i];
+    }
 
-    vector<int> A(N);
-    for (int i = 0; i < N; i++)
-        cin >> A[i];
-
-    solve(A, N, MAX_W);
-
+    solve(
+        people_weights,
+        people_cnt,
+        max_elev_weight
+    );
+    
     return 0;
 }
